d7961a1709ec9a86f7e697e3067be8d774edabe0
Anirban Roy <anirban00537@gmail.com>
2021-09-12 12:21:38 +0600

    added navaber

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <div id="app">
-    <!-- <el-menu
-      :default-active="activeIndex2"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-    >
-      <el-menu-item index="1">
-        <router-link to="/">Home</router-link> |
-      </el-menu-item>
-      <el-menu-item index="2">
-        <router-link to="/about">About</router-link> |
-      </el-menu-item>
-      <router-view />
-    </el-menu> -->
+    <Navbar />
+    <router-view />
   </div>
 </template>
+<script lang="ts">
+import Navbar from "./components/Navbar.vue";
+import { Component, Vue } from "vue-property-decorator";
 
-<style lang="stylus">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+@Component({
+  components: {
+    Navbar,
+  },
+})
+export default class App extends Vue {}
+</script>
